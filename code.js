f7fdce4ffaa989f6823f4c43984f1f73040b93ae
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-09 17:59:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,53 +1,42 @@
 function dijkstra(graph, sourceNode) {
-    let distance = {}
-    return [];
+    let visited = [];
+    let parent = {};
+    let distance = {};
+    let queue = [sourceNode];
+
+    for (let node in graph) {
+        distance[node] = Infinity;
+    }
+
+    distance[sourceNode] = 0;
+
+    while (queue.length > 0) {
+        let min = null;
+
+        for (let node in graph) {
+            if (!visited.includes(node) && (min === null || distance[node] < distance[min])) {
+                min = node;
+            }
+        }
+
+        if (min === null) break;
+
+        visited.push(min);
+
+        for (let i = 0; i < graph[min].length; i++) {
+            const [neighbor, weight] = graph[min][i];
+            let newDistance = distance[min] + weight;
+
+            if (newDistance < distance[neighbor]) {
+                distance[neighbor] = newDistance;
+                parent[neighbor] = min;
+
+                if (!visited.includes(neighbor) && !queue.includes(neighbor)) {
+                    queue.push(neighbor);
+                }
+            }
+        }
+    }
+
+    return distance;
 }
-
-//function breadthFirstSearch(graph, startNode, targetNode) { // note that graph will be an adjacency list
-//     let visited = [];
-//     let parent = {};
-//     let queue = [startNode];
-
-//     while (queue.length > 0) {
-//         let node = queue.shift();
-
-//         if (node === targetNode) {
-//             let path = [];
-//             let currentNode = targetNode;
-//             while (currentNode !== undefined) {
-//                 path.unshift(currentNode);
-//                 currentNode = parent[currentNode];
-//             }
-//             return path;
-//         }
-
-//         if (!visited.includes(node)) {
-//             visited.push(node);
-
-//             for (let i = 0; i < graph[node].length; i++) {
-//                 const neighbor = graph[node][i];
-//                 if (!visited.includes(neighbor) && !queue.includes(neighbor)) {
-//                     queue.push(neighbor);
-//                     parent[neighbor] = node;
-//                 }
-//             }
-//         }
-//     }
-
-//     return [];
-// }
-
-const graph = {
-    0: [[3, 1], [4, 2], [2, 4], [7, 7]],
-    1: [[0, 3]],
-    2: [[1, 1], [4, 3]],
-    3: [[2, 1], [5, 2], [6, 4]],
-    4: [[1, 5], [5, 2]],
-    5: [[6, 1]],
-    6: [],
-    7: [[8, 1]],
-    8: [],
-    9: []
-};
-
-console.log(dijkstra(graph,0));
