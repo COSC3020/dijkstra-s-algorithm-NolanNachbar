1ed023f423597d13af5789625698855cb137d877
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-09 16:18:01 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function dijkstra(graph, sourceNode) {
+    let distance = {}
     return [];
 }
+
+const graph = [
+    [3, 4, 2, 7],
+    [0],
+    [1, 4],
+    [2, 5, 6],
+    [1, 5],
+    [6],
+    [],
+    [8],
+    [],
+    []
+];
+
+console.log(dijkstra(graph,0));
