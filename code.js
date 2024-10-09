2c65984b6c6b746e033e2ecbc5d9796c87e8a852
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-09 16:24:27 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,6 +3,40 @@ function dijkstra(graph, sourceNode) {
     return [];
 }
 
+//function breadthFirstSearch(graph, startNode, targetNode) { // note that graph will be an adjacency list
+//     let visited = [];
+//     let parent = {};
+//     let queue = [startNode];
+
+//     while (queue.length > 0) {
+//         let node = queue.shift();
+
+//         if (node === targetNode) {
+//             let path = [];
+//             let currentNode = targetNode;
+//             while (currentNode !== undefined) {
+//                 path.unshift(currentNode);
+//                 currentNode = parent[currentNode];
+//             }
+//             return path;
+//         }
+
+//         if (!visited.includes(node)) {
+//             visited.push(node);
+
+//             for (let i = 0; i < graph[node].length; i++) {
+//                 const neighbor = graph[node][i];
+//                 if (!visited.includes(neighbor) && !queue.includes(neighbor)) {
+//                     queue.push(neighbor);
+//                     parent[neighbor] = node;
+//                 }
+//             }
+//         }
+//     }
+
+//     return [];
+// }
+
 const graph = {
     0: [[3, 1], [4, 2], [2, 4], [7, 7]],
     1: [[0, 3]],
