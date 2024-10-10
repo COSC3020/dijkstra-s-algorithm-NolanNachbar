a035256c1f155b05194095a2f3ecc2d18e71e72a
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-09 18:07:55 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,57 +3,56 @@ const jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
 // Running tests
-const startNode = 0;
-const targetNode = 7;
-const graph = [
-    [3, 4, 2, 7],
-    [0],
-    [1, 4],
-    [2, 5, 6],
-    [1, 5],
-    [6],
-    [],
-    [8],
-    [],
-    []
-];
+const sourceNode = 0;
 
-const graph2 = [
-    [1, 2],
-    [3],
-    [3, 4],
-    [],
-    [5, 6],
-    [],
-    [7],
-    [],
-    [],
-    []
-];
+const graph = {
+    0: [[3, 2], [4, 1], [2, 1], [7, 5]], 
+    1: [[0, 6]],                          
+    2: [[1, 87], [4, 4]],                  
+    3: [[2, 3], [5, 3], [6, 5]],         
+    4: [[1, 4], [5, 4]],
+    5: [[6, 2]],
+    6: [],
+    7: [[8, 1]],
+    8: [],
+    9: []
+};
 
-const graph3 = [
-    [1],
-    [2, 3],
-    [4],
-    [7],
-    [5],
-    [],
-    [8],
-    []
-];
+const graph2 = {
+    0: [[1, 3], [2, 7]],
+    1: [[3, 2]], 
+    2: [[3, 1], [4, 0]],
+    3: [],
+    4: [[5, 2], [6, 4]],
+    5: [],
+    6: [[7, 4]],
+    7: [],
+    8: [],
+    9: []
+};
 
+const graph3 = {
+    0: [[1, 1]],
+    1: [[2, 5], [3, 12]],
+    2: [[4, 1]],
+    3: [[7, 5]],
+    4: [[5, 2]],
+    5: [],
+    6: [[8, 8]],
+    7: [],
+};
 
 function arraysEqual(arr1, arr2) {
     return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
 const tests = [
-    { func: depthFirstSearch, graph: graph, result: [0, 7], name: 'DFS Test 1' },
-    { func: depthFirstSearch, graph: graph2, result: [0, 2, 4, 6, 7], name: 'DFS Test 2' },
-    { func: depthFirstSearch, graph: graph3, result: [0, 1, 3, 7], name: 'DFS Test 3' },
-    { func: breadthFirstSearch, graph: graph, result: [0, 7], name: 'BFS Test 1' },
-    { func: breadthFirstSearch, graph: graph2, result: [0, 2, 4, 6, 7], name: 'BFS Test 2' },
-    { func: breadthFirstSearch, graph: graph3, result: [0, 1, 3, 7], name: 'BFS Test 3' }
+    { func: dijkstra, graph: graph, result: { '0': 0, '1': 5, '2': 1, '3': 2, '4': 1, '5': 5, '6': 7, '7': 5, '8': 6, '9': 8 }
+, name: "Dijkstra's Test 1" },
+    { func: dijkstra, graph: graph2, result: { '0': 0, '1': 3, '2': 7, '3': 5, '4': 7, '5': 9, '6': 11, '7': 15, '8': 10, '9': 13 }
+, name: "Dijkstra's Test 2" },
+    { func: dijkstra, graph: graph3, result: { '0': 0, '1': 5, '2': 1, '3': 2, '4': 1, '5': 5, '6': 7, '7': 5, '8': 6, '9': 8 }
+, name: "Dijkstra's Test 3" },
 ];
 
 tests.forEach(test => {
